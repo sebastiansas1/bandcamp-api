b2ee27bebd6afa6b09724e8ff7eff6a4a0f99b44
Sebastian Sas Mangel <sebastiansasmangel@rentalcars.com>
2020-04-25 14:28:06 +0100

    set default port from process 2

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const bandcampRouter = require('./routers/bandcampRouter');
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use('/api', bandcampRouter);
 
 app.listen(port, () => {
-  console.log(`Server started on port ${process.env.PORT || port}`);
+  console.log(`Server started on port ${port}`);
 });
